e82ee366c8987cefe6dfc31ca193ea5931a9bfbc
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2013-10-11 15:47:30 +0300

    init feature

---
+++ modules/financial_reports/__openerp__.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+{
+    'name': 'Финансовые отчеты',
+    'version': '0.1',
+    'category': 'Reports',
+    'complexity': "easy",
+    'application': True,
+    'description': """
+Финансовые отчеты.
+==============================
+    """,
+    'author': 'Andrey Karbanovich',
+    'website': 'http://upsale.ru',
+    'depends': ['base', 'account'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'update_xml': [
+        #'views/view_plan.xml',
+    ],
+    'js': [],
+    'css': [],
+    'qweb': [],
+    'images': [],
+}
---
+++ modules/financial_reports/ppc.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from openerp import tools
+from openerp.osv import fields
+from openerp.osv.orm import Model
+
+
+class PPCReport(Model):
+    _name = 'financial.reports.ppc'
+    _description = u'Финансовые отчеты - PPC'
+
+    _columns = {
+
+    }
