68366c5fd85449dd56273a6a5c329909da5fb8b5
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2013-10-28 15:46:46 +0200

    Конпка обновить

--- modules/financial_reports/ppc.py
+++ modules/financial_reports/ppc.py
@@ -453,7 +453,7 @@ class PPCReport(Model):
     }
 
     def save(self, cr, uid, ids, context=None):
-        line_ids = self.pool.get('account.invoice.pay.line').search(cr, 1, [('invoice_id', '!=', False)])
+        line_ids = self.pool.get('account.invoice.pay.line').search(cr, 1, [('invoice_id', '!=', False), ('invoice_id', 'not in', [140])])
         print line_ids
         return self.pool.get('account.invoice.pay.line').write(cr, 1, line_ids, {'close': False})
 PPCReport()
