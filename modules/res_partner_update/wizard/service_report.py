5a7c02e7ebb338c732a1d85ae2f0d4ddc8213682
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2013-10-28 17:05:57 +0200

    Конпка обновить

--- modules/res_partner_update/wizard/service_report.py
+++ modules/res_partner_update/wizard/service_report.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from collections import Counter
+from res_partner_update.counter import Counter
 
 from openerp.osv import fields
 from openerp.osv.orm import TransientModel
