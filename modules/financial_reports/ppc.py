44b252fc435a7023f3589bea1e7824c75c6c58f3
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2013-11-01 13:40:37 +0200

    financial

--- modules/financial_reports/ppc.py
+++ modules/financial_reports/ppc.py
@@ -55,6 +55,7 @@ class PPCReport(Model):
             ('close_date', '>=', date_start),
             ('partner_id', '!=', False),
             ('invoice_id', '!=', False),
+            ('invoice_id.user_id', '!=', 170)
         ]
         pay_line_ids = pay_line_pool.search(cr, 1, domain, order='partner_id ASC, service_id ASC, invoice_date ASC')
         lines = []
