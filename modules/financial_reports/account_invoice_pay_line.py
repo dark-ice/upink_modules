ce27399c3bc42bf0a1d8d3c63e45199d6bb8c10b
Andrey Karbanovich <karbanovich.andrey@gmail.com>
2013-11-05 08:21:24 +0200

    financial

--- modules/financial_reports/account_invoice_pay_line.py
+++ modules/financial_reports/account_invoice_pay_line.py
@@ -41,9 +41,18 @@ class AccountInvoicePayLine(Model):
                                         'specialist_id': process['specialist_id'][0],
                                         'site_url': process['site_url'],
                                     }
+                                    #self.write(cr, 1, [record['id']], {'specialist_id': process['specialist_id'][0], 'site_url': process['site_url']})
                             except:
                                 pass
         return res
+
+    def _search_specialist(self, cr, uid, obj, name, args, context):
+        ids = self.search(cr, uid, [])
+        res = self._get_specialist(cr, uid, ids, '', {})
+        line_ids = [k for k, v in res.iteritems() if v['specialist_id'] == args[0][2]]
+        if line_ids:
+            return [('id', 'in', line_ids)]
+        return [('id', '=', '0')]
 
     def onchange_close(self, cr, uid, ids, close):
         if close:
@@ -63,7 +72,8 @@ class AccountInvoicePayLine(Model):
             relation='res.users',
             string='Аккаунт-менеджер',
             store=True,
-            multi='process'
+            multi='process',
+            fnct_search=_search_specialist
         ),
         'site_url': fields.function(
             _get_specialist,
@@ -92,6 +102,7 @@ class AccountInvoicePayLine(Model):
         'add_revenues': fields.float('Доп. доходы'),
         'add_costs': fields.float('Доп. расходы'),
         'period_id': fields.many2one('kpi.period', 'Период', domain=[('calendar', '=', 'rus')]),
+        'save': fields.boolean('Save')
     }
 
     def search(self, cr, user, args, offset=0, limit=None, order=None, context=None, count=False):
