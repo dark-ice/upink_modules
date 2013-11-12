a82e10dd8c9821a75a410d17c397ab4c896dac0c
skripnik <dark-ice-666@rambler.ru>
2013-11-12 15:36:15 +0200

    начало работ по созданию отчета удовлетворенности услуг

--- modules/reports_quality_control/report_quality_control_manager.py
+++ modules/reports_quality_control/report_quality_control_manager.py
@@ -1,15 +1,18 @@
 # coding=utf-8
+from openerp.osv import fields
 from openerp.osv.orm import TransientModel
 
 
 class ReportQualityControlManager(TransientModel):
     _name = "report.quality.control.manager"
     _columns = {
-        'manager_id'
-        'service_count'
-        'quality_point'
-        'mbo'
-        'quality_index'
+        'manager_id': fields.many2one('res.users', 'Менеджер'),
+        'service_count': fields.integer('Количество оцененнызх услуг'),
+        'quality_point': fields.integer('Уровень удовлетворенности'),
+        'mbo': fields.integer('MBO по услуге'),
+        'quality_index': fields.integer('Индекс удевлетвореннносте'),
+        'date_start': fields.date('Дата начала'),
+        'date_finish': fields.date('Дата окончания'),
     }
 
 ReportQualityControlManager()
