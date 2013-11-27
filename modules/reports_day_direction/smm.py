05a09bde5d1ec5535e0d7e82fcdbc532ec90e0ad
skripnik <dark-ice-666@rambler.ru>
2013-11-27 11:44:11 +0200

    начало работы с отчетами smm

--- modules/reports_day_direction/smm.py
+++ modules/reports_day_direction/smm.py
@@ -5,10 +5,21 @@ from openerp.osv.orm import Model
 
 class ReportDaySmmStatisticPlan(Model):
     _name = "report.day.smm.static.plan"
-    _description = u"доп модель отчета по направлению smm"
+    _description = u"доп модель отчета по направлению smm план"
     _columns = {
         'process_smm_id': fields.many2one('process.call.in', 'Проект', domain="[('state', '=', 'development')]"),
 
     }
 
 ReportDaySmmStatisticPlan()
+
+
+class ReportDaySmmStatisticFact(Model):
+    _name = "report.day.smm.static.fact"
+    _description = u"доп модель отчета по направлению smm факт"
+    _columns = {
+        'process_smm_id': fields.many2one('process.call.in', 'Проект', domain="[('state', '=', 'development')]"),
+
+    }
+
+ReportDaySmmStatisticFact()
