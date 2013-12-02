b18a0519be233c065873e49b26286dfe3865f636
skripnik <dark-ice-666@rambler.ru>
2013-12-02 09:25:22 +0200

    отчет по site 80%

--- modules/reports_day_direction/site.py
+++ modules/reports_day_direction/site.py
@@ -3,6 +3,14 @@ from openerp import tools
 from openerp.osv import fields
 from openerp.osv.orm import Model
 
+STAGES = (
+    ('planning', 'Проектирование'),
+    ('design', 'Дизайн'),
+    ('makeup', 'Верстка'),
+    ('developing', 'Программирование'),
+    ('testing', 'Тестирование'),
+)
+
 
 class ReportDaySite(Model):
     _name = "report.day.site"
@@ -10,31 +18,34 @@ class ReportDaySite(Model):
     _auto = False
     _columns = {
         'partner_id': fields.many2one('res.partner', "Проект"),
-        'stage': fields.char('этап', size=128),
-        'now_stage': fields.char('Текущий этап', size=128),
-        'now_plan_fn': fields.date('Планируемая дата завершения текущего этапа'),
+        'stage': fields.selection(STAGES, 'Этап'),
+        'now_stage': fields.selection(STAGES, 'Текущий этап'),
+        'now_plan_fn': fields.date('Дата завершения'),
         'plan_date_st': fields.date('Планируемая дата начала'),
         'plan_date_fn': fields.date('Планируемая дата окончания'),
         'real_date_st': fields.date('Фактическая дата начала'),
         'real_date_fn': fields.date('Фактическая дата окончания'),
         'date_end': fields.date('Production'),
+        'days': fields.integer('Длительность')
     }
+
     def init(self, cr):
         tools.drop_view_if_exists(cr, 'report_day_site')
         cr.execute("""
             create or replace view report_day_site as (
                 SELECT
                     row_number() over() as id,
-                      pl.partner_id,
-                      pss.stage,
-                      pss.plan_date_st,
-                      pss.plan_date_fn,
-                      pss.real_date_st,
-                      pss.real_date_fn,
-                      ps.date_end,
+                        pl.partner_id,
+                        pss.stage,
+                        pss.plan_date_st,
+                        pss.plan_date_fn,
+                        pss.real_date_st,
+                        pss.real_date_fn,
+                        ps.date_end,
+                        (pss.real_date_fn - pss.real_date_st) as days,
 
-                      asd.plan_date_fn as now_plan_fn,
-                      asd.stage as now_stage
+                        asd.plan_date_fn as now_plan_fn,
+                        asd.stage as now_stage
 
                     from process_site ps
                     join process_launch pl
