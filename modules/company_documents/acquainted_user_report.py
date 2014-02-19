2ce7361c6d18f080e76db343c7aeec6b2b4773bf
skripnik <dark-ice-666@rambler.ru>
2014-02-19 09:31:46 +0200

    список ознакомившихся

--- modules/company_documents/acquainted_user_report.py
+++ modules/company_documents/acquainted_user_report.py
@@ -22,38 +22,38 @@ class AcquaintedUserReport(Model):
         ),
     }
 
-    # def init(self, cr):
-    #     tools.drop_view_if_exists(cr, 'acquainted_user_report')
-    #     cr.execute("""
-    #         create or replace view acquainted_user_report as (
-    #         select
-    #           row_number()
-    #           OVER () AS id,
-    #           doc_id,
-    #           user_id,
-    #           label
-    #         from(
-    #               select
-    #               see_u.doc_id,
-    #               see_u.user_id,
-    #               CONCAT( CASE WHEN ((select au.user_id from acquainted_users as au where au.user_id = see_u.user_id and au.disposition_id = see_u.doc_id) is null) then 'no' else 'yes' end) as label
-    #             from
-    #               see_company_disposal_users_rel as see_u
-    #
-    #             union
-    #
-    #             select
-    #               see_g.doc_id,
-    #               st_g.usr_id as user_id,
-    #               CONCAT( CASE WHEN  ((select au.user_id from acquainted_users as au where au.user_id = st_g.usr_id and au.disposition_id = see_g.doc_id) is null) then 'no' else 'yes' end) as label
-    #             from
-    #               see_company_disposal_groups_rel as see_g
-    #             join res_user_storage_groups_rel as st_g
-    #             on (st_g.gro_id = see_g.group_id)
-    #             order by doc_id
-    #
-    #         ) r
-    #         )""")
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'acquainted_user_report')
+        cr.execute("""
+            create or replace view acquainted_user_report as (
+            select
+              row_number()
+              OVER () AS id,
+              doc_id,
+              user_id,
+              label
+            from(
+                  select
+                  see_u.doc_id,
+                  see_u.user_id,
+                  CONCAT( CASE WHEN ((select au.user_id from acquainted_users as au where au.user_id = see_u.user_id and au.disposition_id = see_u.doc_id) is null) then 'no' else 'yes' end) as label
+                from
+                  see_company_disposal_users_rel as see_u
+
+                union
+
+                select
+                  see_g.doc_id,
+                  st_g.usr_id as user_id,
+                  CONCAT( CASE WHEN  ((select au.user_id from acquainted_users as au where au.user_id = st_g.usr_id and au.disposition_id = see_g.doc_id) is null) then 'no' else 'yes' end) as label
+                from
+                  see_company_disposal_groups_rel as see_g
+                join res_user_storage_groups_rel as st_g
+                on (st_g.gro_id = see_g.group_id)
+                order by doc_id
+
+            ) r
+            )""")
 
 
 AcquaintedUserReport()
